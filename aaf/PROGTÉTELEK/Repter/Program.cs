dd6c2af6ff2aba467ad296cb14fa7fe7da7ffc36
magda.peter <magda.peter@ckik.hu>
2024-11-20 10:32:52 +0100

    ujcommit

--- aaf/PROGTÉTELEK/Repter/Program.cs
+++ aaf/PROGTÉTELEK/Repter/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -40,6 +41,21 @@ namespace Repter
                 30, 
                 24 
                 };
+            
+            //double[] tomegek =
+            //    {
+            //    22.3,
+            //    120,
+            //    70.5,
+            //    180.44,
+            //    35, 28,
+            //    140.12,
+            //    80,
+            //    160.9,
+            //    50.8,
+            //    30,
+            //    24
+            //    };
 
             int i;
             int n = tomegek.Length;
@@ -72,7 +88,141 @@ namespace Repter
                 Console.WriteLine("2.\nNincs ilyen!\n");
             }
 
-            //F3
+            //F3 - Feltételes átlag = Összegzés + Megszámolás
+            double osszeg = 0;
+            int db = 0;
+
+            for (i = 0; i < n; i++)
+            {
+
+                if (tomegek[i] < 20)
+                {
+                    osszeg += tomegek[i];
+                    db++;
+                }
+                else
+                {
+                    
+                }
+            }
+            
+            if (db == 0)
+            {
+                Console.WriteLine("3.\n Nincs 20 kg alatti csomag\n");
+            }
+            else
+            {
+                double atlag = osszeg / db;
+                Console.WriteLine($"3.\n{atlag:0.00} kg\n");
+            }
+
+            //F4 - Eldöntés
+            i = 0;
+            while (i < n && !(tomegek[i] <= 5))
+            {
+                i++;
+            }
+
+            if (i < n)
+            {
+                Console.WriteLine("4.\n Nem igaz!\n");
+            }
+            else
+            {
+                Console.WriteLine("4.\n Igaz!\n");
+            }
+
+            //F5
+            db = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (nevek[i].Length > 10)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine($"5.\n{db} név hosszab 10 karakternél\n");
+
+            //F6
+            db = 0;
+            for (i = 0; i < n - 1; i++)
+            {
+                if (tomegek[i] > tomegek[i + 1])
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine($"6.\n{db}\n");
+
+            //F7 
+            i = 0;
+            while (nevek[i] != "Farkas Dóra")
+            {
+                i++;
+            }
+            double dora = tomegek[i];
+            db = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (tomegek[i] < dora)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine($"7.\n{db}\n");
+
+            //F8 - Feltételes maximum
+
+
+            //1. Megoldás
+
+            //int maxi = 0;
+            //double maxe = -1 ;
+            //for (i = 1; i < n; i++)
+            //{
+            //    if (tomegek[i] > maxe && tomegek[i] > 9 && tomegek[i] < 21)
+            //    {
+            //        maxi = i;
+            //        maxe = tomegek[i];
+            //    }
+            //}
+            //if (maxe == -1)
+            //{
+            //    Console.WriteLine($"8.\nNincs megfelelő\n");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"8.\n{nevek[maxi]}\n");
+            //}
+
+
+            //2.Megoldás - Keresés + Maximum kiválasztás
+
+            i = 0;
+            while (i < n && !(9 < tomegek[i] && tomegek[i] < 21))
+            {
+                i++;
+            }
+            if (i < n)
+            {
+                int maxi = i;
+                for (int j = i; j < n; j++)
+                {
+                    if (tomegek[j] > tomegek[maxi] && 9 < tomegek[j] && tomegek[j] < 21)
+                    {
+                        maxi = j;
+                    }
+                }
+                Console.WriteLine($"8.\n{nevek[maxi]}\n");
+            }
+            else
+            {
+                Console.WriteLine($"8.\nNincs megfelelő\n");
+            }
+
+
+
+
             Console.ReadKey();
         }
     }
---
+++ aaf/PROGTÉTELEK/jogositvany/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace jogositvany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Statikus tömbök: fordítási időben tudnunk kell a méretét!
+            // Ez így NEM JÓ!!!
+            // string[] nevek = new string[n];
+            string[] nevek = new string[100];
+            int[] korok = new int[100];
+            bool[] jogsik = new bool[100]; // jogsik[i]: az i. adatnak van-e jogsija?
+
+            //F1 - Beolvasás
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                string sor = Console.ReadLine(); //"Daniel 30 I"
+                string[] adatok = sor.Split(' '); //{"Daniel", "30", "I"}
+                nevek[i] = adatok[0]; //{ "Daniel", "Marta", ...}
+                korok[i] = Convert.ToInt32(adatok[1]);
+                
+                //1.
+                //if (adatok[2] == "I") jogsik[i] = true;
+                //else jogsik[i] = false;
+
+                //2.
+                //jogsik[i] = adatok[2] == "I" ? true : false; // ternary operator
+
+                //3.
+                jogsik[i] = adatok[2] == "I";
+            }
+            Console.WriteLine(jogsik[0]);
+            
+            //Console.ReadKey();
+        }
+    }
+}
